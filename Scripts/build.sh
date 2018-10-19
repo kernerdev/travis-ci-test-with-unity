702c39044ed344b3f3c01368934f65372f97dc27
Bence Kerner <kernerdev@gmail.com>
2018-10-20 00:00:24 +0200

    update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -20,29 +20,6 @@ echo "Attempting to build $project for Windows"
   -editorTestsResultFile $(pwd)/travis/test.xml \
   -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/travis/unity.log \
-  -projectPath $(pwd)/travis \
-  -buildOSXUniversalPlayer "$(pwd)/travis/Build/osx/$project.app" \
-  -runEditorTests \
-  -editorTestsResultFile $(pwd)/travis/test.xml \
-  -quit
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/travis/unity.log \
-  -projectPath $(pwd)/travis \
-  -buildLinuxUniversalPlayer "$(pwd)/travis/Build/linux/$project.exe" \
-  -runEditorTests \
-  -editorTestsResultFile $(pwd)/travis/test.xml \
-  -quit
 
 echo 'Logs from build'
 cat $(pwd)/travis/unity.log
