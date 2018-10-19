b5328830d383ccdbd430fb038e50c95517607ce2
Bence Kerner <kernerdev@gmail.com>
2018-10-19 23:53:56 +0200

    update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 project="travis"
+echo "Start build.sh"
+echo $(pwd) 
+ls -l
+cd travis
+ls -l
+cd ..
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -10,6 +16,8 @@ echo "Attempting to build $project for Windows"
   -logFile $(pwd)/travis/unity.log \
   -projectPath $(pwd)/travis \
   -buildWindowsPlayer "$(pwd)/travis/Build/windows/$project.exe" \
+  -runEditorTests \
+  -editorTestsResultFile $(pwd)/travis/test.xml \
   -quit
 
 echo "Attempting to build $project for OS X"
@@ -20,6 +28,8 @@ echo "Attempting to build $project for OS X"
   -logFile $(pwd)/travis/unity.log \
   -projectPath $(pwd)/travis \
   -buildOSXUniversalPlayer "$(pwd)/travis/Build/osx/$project.app" \
+  -runEditorTests \
+  -editorTestsResultFile $(pwd)/travis/test.xml \
   -quit
 
 echo "Attempting to build $project for Linux"
@@ -30,6 +40,8 @@ echo "Attempting to build $project for Linux"
   -logFile $(pwd)/travis/unity.log \
   -projectPath $(pwd)/travis \
   -buildLinuxUniversalPlayer "$(pwd)/travis/Build/linux/$project.exe" \
+  -runEditorTests \
+  -editorTestsResultFile $(pwd)/travis/test.xml \
   -quit
 
 echo 'Logs from build'
