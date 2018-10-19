3d784f153681c30141b6db5962aab2933448f471
Unknown <weiszkristof@gmail.com>
2018-10-19 19:27:33 +0200

    DummyBackEnd Solution with nUnit

---
+++ DummyBackendDotNetStandard/DummyBackendDotNetStandard_Test/DummyBackend_uTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DummyBackendDotNetStandard;
+using NUnit.Framework;
+
+namespace DummyBackendDotNetStandard_Test
+{
+    [TestFixture]
+    class DummyBackend_uTest
+    {
+        [TestCase(1, 1, 2)]
+        [TestCase(10, 10, 20)]
+        [TestCase(0, 0, 0)]
+        public void AddTest(int a, int b, int result)
+        {
+            Assert.AreEqual(result, DummyBackend.Add(a, b), "Result is not as expected");
+        }
+
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -1)]
+        public void AddTest_Exception(int a, int b)
+        {
+            Assert.Throws<ArgumentException>(() => DummyBackend.Add(a, b), "Exception not thrown");
+        }
+    }
+}
