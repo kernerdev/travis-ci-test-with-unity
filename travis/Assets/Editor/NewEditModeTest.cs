544cad898d74f99d3b04079896ee496043e0c436
Bence Kerner <kernerdev@gmail.com>
2018-10-20 12:02:13 +0200

    try with a passed test (1 == 1)

---
+++ travis/Assets/Editor/NewEditModeTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+public class NewEditModeTest {
+
+    [Test]
+    public void Pass()
+    {
+        // var fileSystem = new FileSystem();
+        Assert.AreEqual(1, 1);
+    }
+}
