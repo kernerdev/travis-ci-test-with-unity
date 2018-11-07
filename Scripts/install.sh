0c159fa339856542c33b7f2e7d28ec2ffafd809a
Bence Kerner <kernerdev@gmail.com>
2018-11-07 23:01:50 +0100

    asd

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+ls -l
 cd ..
+ls -l
 mkdir unityEditor
 cd unityEditor
+ls -l
 wget -O UnitySetup "https://beta.unity3d.com/download/dad990bf2728/UnitySetup-2018.2.7f1"
+ls -l
 sudo chmod 777 UnitySetup
-sudo ./UnitySetup -u  --install-location="$(pwd)" <<< 'y'
+ls -l
+sudo ./UnitySetup -u  --install-location=$(pwd) <<< 'y'
+ls -l
 cd Editor
+ls -l
 cat "it is enough now"
 #sudo ./Unity -batchmode -projectPath "/home/kerner/New Unity Project" -runTests -testResults "/home/kerner/results.xml" -testPlatform editmode -nographics
