29c366491a9dee71700bfdbbdaa095af7a4f623d
Bence Kerner <kernerdev@gmail.com>
2018-11-07 23:40:11 +0100

    asd

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo apt-get --yes --force-yes install gconf-service lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf libproxy1 glib-networking-services
+sudo apt-get --yes --force-yes install gconf-service lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf glib-networking-services
 sudo apt-get --yes --force-yes install glib-networking
 sudo apt-get --yes --force-yes install libsoup2.4-1
 ls -l
