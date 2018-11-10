9d27c675c193e11e74837965f62b7c8a1a24dc3a
Bence Kerner <kernerdev@gmail.com>
2018-11-10 22:04:51 +0100

    asd

--- Travis/unity_test_parser.sh
+++ Travis/unity_test_parser.sh
@@ -3,11 +3,15 @@
 total=$(grep -o -a -m 1 -h -r "total=\"[0-9]*\"" "$1")
 passed=$(grep -o -a -m 1 -h -r "passed=\"[0-9]*\"" "$1")
 failed=$(grep -o -a -m 1 -h -r "failed=\"[0-9]*\"" "$1")
-total_count=$(echo $total | grep -o "[0-9]*")
-passed_count=$(echo $passed | grep -o "[0-9]*")
-failed_count=$(echo $failed | grep -o "[0-9]*")
-echo "\n\n\n\n"
-echo "--------------------------------------"
+echo $total
+echo $passed
+echo $failed
+total_count=$(echo $total | grep -o -E "\d"+)
+passed_count=$(echo $passed | grep -o -E "\d"+)
+failed_count=$(echo $failed | grep -o -E "\d"+)
+echo $total_count
+echo $passed_count
+echo $failed_count
 if [ "$total_count" -eq "$passed_count" ] || [ "$failed_count" -eq "0" ]; then
     echo "TEST PASSED"
     echo "There are $total_count test."
@@ -18,5 +22,3 @@ else
     echo "There are $passed_count passed test."
     echo "There are $failed_count failed test."
 fi
-echo "--------------------------------------"
-echo "\n\n\n\n"
