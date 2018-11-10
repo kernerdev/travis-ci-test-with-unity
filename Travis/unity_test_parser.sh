5b4c8e8d473d73ee5f07dfbb810060d45384b7b0
Bence Kerner <kernerdev@gmail.com>
2018-11-10 21:01:13 +0100

    change again because of macos perl incompatibility

--- Travis/unity_test_parser.sh
+++ Travis/unity_test_parser.sh
@@ -1,11 +1,11 @@
-#! /bin/sh
-
-total=$(grep -o -a -m 1 -P -h -r 'total="\d"' "$1")
-passed=$(grep -o -a -m 1 -P -h -r 'passed="\d"' "$1")
-failed=$(grep -o -a -m 1 -P -h -r 'failed="\d"' "$1")
-total_count=$(echo $total | grep -oP '\d')
-passed_count=$(echo $passed | grep -oP '\d')
-failed_count=$(echo $failed | grep -oP '\d')
+#!/bin/sh
+ 
+total=$(grep -o -a -m 1 -h -r "total=\"[0-9]*\"" "$1")
+passed=$(grep -o -a -m 1 -h -r "passed=\"[0-9]*\"" "$1")
+failed=$(grep -o -a -m 1 -h -r "failed=\"[0-9]*\"" "$1")
+total_count=$(echo $total | grep -o "[0-9]*")
+passed_count=$(echo $passed | grep -o "[0-9]*")
+failed_count=$(echo $failed | grep -o "[0-9]*")
 echo "\n\n\n\n"
 echo "--------------------------------------"
 if [ "$total_count" -eq "$passed_count" ] || [ "$failed_count" -eq "0" ]; then
