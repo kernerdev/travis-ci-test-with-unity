6625c07fc99b76bef32b500b4506a91704dabed4
Bence Kerner <kernerdev@gmail.com>
2018-11-17 01:46:29 +0100

    asd

--- Travis/tag.sh
+++ Travis/tag.sh
@@ -3,7 +3,14 @@
 #git config --local user.name "traviscibot"
 #git config --local user.email "traviscibot@travisci.org"
 echo "$TRAVIS_BRANCH"
+cd ..
+mkdir asd
+cd asd
+git clone --depth=50 --branch=xunit_dotnet_core https://github.com/kernerdev/travis-ci-test-with-unity.git
+cd travis-ci-test-with-unity
 ls -l
+sudo chmod a+x ./Travis/tag.sh
+sudo chmod a+x ./Travis/tag_incrementer.sh
 sudo ./Travis/tag_incrementer.sh $TRAVIS_BRANCH version.txt
 version=$(cat version.txt)
 git add version.txt
