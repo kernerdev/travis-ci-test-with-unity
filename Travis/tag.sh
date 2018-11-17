531221e8dd08bb52970c3137c91df978466673bb
Bence Kerner <kernerdev@gmail.com>
2018-11-17 01:42:39 +0100

    asd

--- Travis/tag.sh
+++ Travis/tag.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-git config --local user.name "traviscibot"
-git config --local user.email "traviscibot@travisci.org"
+#git config --local user.name "traviscibot"
+#git config --local user.email "traviscibot@travisci.org"
 echo "$TRAVIS_BRANCH"
 ls -l
 sudo ./Travis/tag_incrementer.sh $TRAVIS_BRANCH version.txt
