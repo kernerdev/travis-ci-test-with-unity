0b3b4de13816a729e8f27273ff9e3405fe8752f4
Bence Kerner <kernerdev@gmail.com>
2018-11-17 01:16:06 +0100

    asd

--- Travis/tag.sh
+++ Travis/tag.sh
@@ -2,9 +2,9 @@
 
 git config --local user.name "traviscibot"
 git config --local user.email "traviscibot@travisci.org"
-./tag_incrementer.sh $TRAVIS_BRANCH ../versions.txt 
-./Travis/tag_incrementer.sh $TRAVIS_BRANCH ../versions.txt
-version=$(cat ../versions.txt)
+./tag_incrementer.sh $TRAVIS_BRANCH version.txt 
+./Travis/tag_incrementer.sh $TRAVIS_BRANCH version.txt
+version=$(cat version.txt)
 git add ../version.txt
 git commit -m "version update $version"
 git push
